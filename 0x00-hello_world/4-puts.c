1955b25179b3bfc908d3cdd18b9accfdcc64b094
Chipo Hameja <chipohameja@gmail.com>
2023-08-21 14:03:47 -0700

    Another commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - demonstrates the usage of the puts() function.
+ * @void: no parameters accepted
+ * It prints a string to the console.
+ * Return: 0  Program executed successfully.
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
