f464e7619c0a141bac2178f08961e2324f4714e1
Chipo Hameja <chipohameja@gmail.com>
2023-09-25 17:49:19 +0000

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - point of execution.
+ *@void: no parameters accepted
+ *Prints putchar
+ *Return: 0  Program executed successfully.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
