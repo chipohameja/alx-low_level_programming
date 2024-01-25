b733eb31e153b847b348135c033ce73d8200b649
Chipo Hameja <chipohameja@gmail.com>
2024-01-25 14:17:24 +0000

    Some optimization

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if(array != NULL && size > 0 && action != NULL)
+	if (array != NULL && size > 0 && action != NULL)
 	{
 		while (i < size)
 		{
