8c8a72cf28e3f440848334e089d5facbe96e56f8
Chipo Hameja <chipohameja@gmail.com>
2024-01-25 14:07:56 +0000

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterate array
+ * @array: Array to iterate
+ * @size: Size of array to iterate
+ * @action: Iterating callback function
+ * 
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int)) {
+    unsigned int i = 0;
+    
+    while (i < size)
+    {
+        action(array[i]);
+
+        i++;
+    }
+}
