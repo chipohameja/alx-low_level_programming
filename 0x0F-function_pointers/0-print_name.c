aca5d26ca0cac7074e695e7ae40facbd784502ae
Chipo Hameja <chipohameja@gmail.com>
2024-01-25 10:12:14 +0000

    Betty compliant

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,12 @@
  * print_name - prints name
  * @name: name to print
  * @f: pointer to a function
- * 
+ *
  * Return: nothing
  */
 
 void print_name(char *name, void(*f)(char *))
 {
-    if (name != NULL && f != NULL)
-        f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
