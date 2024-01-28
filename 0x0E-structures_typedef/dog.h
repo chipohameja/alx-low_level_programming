e88b3309aa8fd8ed82ba93d7cb79a1428f0b1d4e
Chipo Hameja <chipohameja@gmail.com>
2024-01-28 21:19:39 +0000

    Dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: A structure for dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
