ff318895e0639f68650bffdb07e47cf6d7dedb12
Chipo Hameja <chipohameja@gmail.com>
2024-02-16 22:51:49 +0000

    Betty compliant

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,21 +4,21 @@
 
 /**
  * main - main function (entry point)
- * Description: Print sum of all natural numbers that are multiples of 3 and 5 below 1024
- * Returns: 0
+ * Description: Print sum of natural numbers, multiples of 3 and 5 below 1024
+ * Return: 0
  */
 int main(void)
 {
 	int sum = 0, i = 0;
 
-    while(i < 10)
-    {
-        if (i%3 == 0)
-            sum += i;
-        if  (i%5 == 0)
-            sum += i;
-        i++;
-    }
+	while (i < 10)
+	{
+		if (i % 3 == 0)
+			sum += i;
+		if  (i % 5 == 0)
+			sum += i;
+		i++;
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 }
