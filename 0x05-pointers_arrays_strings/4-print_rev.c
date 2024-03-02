12b089fe1121e0e3a34ad4abf5e77a51c203364e
Chipo Hameja <chipohameja@gmail.com>
2024-03-02 19:36:14 +0000

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints string in reverse
+ * @s: String to print in reverse
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+
+		i--;
+	}
+
+	_putchar('\n');
+}
