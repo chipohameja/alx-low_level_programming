a2aa5a93a3186625788591feff5740f7f4c72a21
Chipo Hameja <chipohameja@gmail.com>
2024-03-02 18:23:01 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes pointer to int as parameter and updates value to 98
+ * @n: Pointer to int that stores the pointer being taken
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
