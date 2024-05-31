7e4ec23ab5a46a8c24d56cc285cbc5da5c828f7f
Chipo Hameja <chipohameja@gmail.com>
2024-05-31 17:55:47 +0000

    Print second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Prints half of a string
+ * @str: String whose half to print
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	len++;
+
+	printf("Length of string is: %d\n", len);
+
+	if (len % 2 > 0)
+		i = (len  - 1) / 2;
+	else
+		i = (len / 2);
+
+	while (i < len)
+	{
+		printf("%c", str[i]);
+
+		i++;
+	}
+
+	printf("\n");
+}
