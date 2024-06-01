b09f63489bfd30bb51abcec22a0c894297e4c0af
Chipo Hameja <chipohameja@gmail.com>
2024-06-01 11:00:11 +0000

    Copy n bytes of string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies n bytes of src to dest
+ * @dest: Final copied string
+ * @src: String copied from
+ * @n: How many bytes of n to copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
